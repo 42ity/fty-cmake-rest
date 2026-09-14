94f4cd4c48d201bbee5ce228ba35970375be550e
François-Régis Degott <francoisregisdegott@eaton.com>
2026-09-14 14:40:53 +0200

    cmake/disable-logtrace.sh : Suppress compilation warnings for unused parameters
    Added [[maybe_unused]] to constructor parameters to suppress warnings.

--- cmake/disable-logtrace.sh
+++ cmake/disable-logtrace.sh
@@ -9,6 +9,9 @@ if [ -z "$file" ]; then echo "file is empty"; exit 0; fi
 # incompatibility with our fty-common-logging macros
 sed -i -E '/^[ \t]*log_trace\(\"/ s/^#*/\/\/CXX_LOGTRACE_DISABLED/' "$file"
 
+# make [[maybe_unused]] arguments in _component_ constructor to avoid compilation warnings
+sed -i 's|(tnt::HttpRequest& request, tnt::HttpReply& reply, tnt::QueryParams& qparam)|([[maybe_unused]] tnt::HttpRequest\& request, [[maybe_unused]] tnt::HttpReply\& reply, [[maybe_unused]] tnt::QueryParams\& qparam)|g' "$file"
+
 ##cat "$file" | grep "log_trace"
 
 exit 0
